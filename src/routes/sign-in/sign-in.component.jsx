0935f95237e965f38eecf1a36225f5a306e08bce
林欣芳 <kingfafa8520@gmail.com>
2022-10-13 12:26:29 +0800

    Sign up via firebase

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,3 +1,4 @@
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 import {
     signInWithGooglePopup,
     createUserDocumentFromAuth,
@@ -15,7 +16,7 @@ const SignIn = () => {
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-        
+            <SignUpForm />
         </div>
     );
 };
